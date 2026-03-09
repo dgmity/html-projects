af5a938070ecd3c498bec1f7b9380702efc94ef3
dgmity <152909763+dgmity@users.noreply.github.com>
2026-03-09 11:44:39 +0100

    add new project

---
+++ arkusz-visibility/pliki7/pliki7/script.js
@@ -0,0 +1,35 @@
+function changeStyleFirst() {
+    document.getElementById("left-1").style.backgroundColor = "mistyrose";
+    document.getElementById("left-2").style.backgroundColor = "#FFAEA5";
+    document.getElementById("left-3").style.backgroundColor = "#FFAEA5 ";
+    document.getElementById("right-1").style.display = "block";
+    document.getElementById("right-1").style.visibility = "visible";
+    document.getElementById("right-2").style.display = "none";
+    document.getElementById("right-2").style.visibility = "hidden";
+    document.getElementById("right-3").style.display = "none";
+    document.getElementById("right-3").style.visibility = "hidden";
+}
+
+function changeStyleSecond() {
+    document.getElementById("left-2").style.backgroundColor = "mistyrose";
+    document.getElementById("left-1").style.backgroundColor = "#FFAEA5";
+    document.getElementById("left-3").style.backgroundColor = "#FFAEA5 ";
+    document.getElementById("right-2").style.display = "block";
+    document.getElementById("right-2").style.visibility = "visible";
+    document.getElementById("right-1").style.display = "none";
+    document.getElementById("right-1").style.visibility = "hidden";
+    document.getElementById("right-3").style.display = "none";
+    document.getElementById("right-3").style.visibility = "hidden";
+}
+
+function changeStyleThird() {
+    document.getElementById("left-3").style.backgroundColor = "mistyrose";
+    document.getElementById("left-1").style.backgroundColor = "#FFAEA5";
+    document.getElementById("left-2").style.backgroundColor = "#FFAEA5 ";
+    document.getElementById("right-3").style.display = "block";
+    document.getElementById("right-3").style.visibility = "visible";
+    document.getElementById("right-2").style.display = "none";
+    document.getElementById("right-2").style.visibility = "hidden";
+    document.getElementById("right-1").style.display = "none";
+    document.getElementById("right-1").style.visibility = "hidden";
+}
